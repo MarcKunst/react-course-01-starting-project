ef5276e1ab499bb16d43912e7df31ca4ea496e4d
MarcKunst <marckunst93@gmail.com>
2025-07-28 20:49:25 +0200

    state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
-    function handleSelect(selectedButton) {
+  const [ selectedTopic, setSelectedTopic ] = useState();
 
-      console.log(selectedButton);
-    }
+  function handleSelect(selectedButton) {
+
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    )
+
+  }
 
   return (
     <div>
@@ -30,6 +52,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
